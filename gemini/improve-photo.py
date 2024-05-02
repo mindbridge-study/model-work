7e954dbe4a29085a6b0c27ab895088a9f714609f
reebxu459 <rebbi.xu123@gmail.com>
2024-05-01 22:28:14 -0400

    improve photo function

--- gemini/improve-photo.py
+++ gemini/improve-photo.py
@@ -1,12 +1,28 @@
 import google.generativeai as genai
 import os
 from dotenv import dotenv_values
+import PIL.Image
 
-os.chdir('..')
-config = dotenv_values(".env")  # (format) config = {"USER": "foo", "EMAIL": "foo@example.org"}
+def improve_photo(photo_path = 'gemini/notebooks/pfp.jpeg'):
+    os.chdir('..')
+    config = dotenv_values(".env")  # (format) config = {"USER": "foo", "EMAIL": "foo@example.org"}
 
-genai.configure(api_key=config["GOOGLE_API_KEY"])
+    genai.configure(api_key=config["GOOGLE_API_KEY"])
 
-model = genai.GenerativeModel('gemini-1.0-pro-latest')
-response = model.generate_content("The opposite of hot is")
-print(response.text)
+    model = genai.GenerativeModel('gemini-pro-vision')
+
+    img = PIL.Image.open(photo_path) #NOTE: jpg does not work
+    prompt = """
+            your goal is to be a robot that helps improve photos. Based on the type of photo, respond ONLY with one of the following options:
+            1. Move closer to the subject.
+            2. Move further from the subject.
+            3. Use the rule of thirds on the subject.
+            4. Move the camera up.
+            5. Move the camera down.
+            6. Move the camera left.
+            7. Move the camera right.
+            """
+    response = model.generate_content([prompt, img])
+
+    print(response.parts)
+
